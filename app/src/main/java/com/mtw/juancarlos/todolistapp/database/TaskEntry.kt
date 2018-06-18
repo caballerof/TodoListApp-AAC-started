a39e0bceeeef67fa5eb26ebac78b90628183ea67
juancarlosgallegos <juan.gallegos@bachoco.net>
2018-06-18 18:56:31 -0500

    KAsyEntry data class TODO annotations

--- app/src/main/java/com/mtw/juancarlos/todolistapp/database/TaskEntry.kt
+++ app/src/main/java/com/mtw/juancarlos/todolistapp/database/TaskEntry.kt
@@ -3,5 +3,10 @@ package com.mtw.juancarlos.todolistapp.database
 import java.util.*
 
 // TODO (2) Annotate the class with Entity. Use "task" for the table name
-data class TaskEntry (var id:Long = 0 ,var description:String, var priority:Int, var updatedAt:Date)
+data class TaskEntry (
+        var id:Long = 0 ,
+        var description:String,
+        var priority:Int,
+        var updatedAt:Date
+)
 // TODO (3) Annotate the id as PrimaryKey. Set autoGenerate to true.
